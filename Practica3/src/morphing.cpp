c0567482fc7486a5be79fda2a3a4fa1a2fffd247
Adolfo Martínez <64854235+ado-marol@users.noreply.github.com>
2024-10-07 13:03:05 +0200

    Add files via upload

---
+++ Practica3/src/morphing.cpp
@@ -0,0 +1,14 @@
+#include "image.h"
+#include "video.h"
+#include <iostream>
+#include <cassert>
+using namespace std;
+Video Morphing(const Image &I1,const Image &I2,int nframes){
+ //RELLENAR POR EL ESTUDIANTE
+//wowowowow
+}
+
+
+int main(int argc, char * argv[]){
+//RELLENAR POR EL ESTUDIANTE
+}
